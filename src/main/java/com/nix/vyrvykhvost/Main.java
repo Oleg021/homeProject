9f040f687526d4fed2094fc21d516992b5610c85
olehvyrvy <olehv208@gmail.com>
2022-06-28 14:14:28 +0300

    first homework on git

---
+++ src/main/java/com/nix/vyrvykhvost/Main.java
@@ -0,0 +1,12 @@
+package com.nix.vyrvykhvost;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hi!! my name is Oleh");
+    }
+}
