94dffab5b55adf58d6495c5bc7a96ddc7b307ac5
olehvyrvy <olehv208@gmail.com>
2022-07-31 13:40:04 +0300

    lesson 12 homework. [rebuild]

--- src/main/java/com/nix/vyrvykhvost/Main.java
+++ src/main/java/com/nix/vyrvykhvost/Main.java
@@ -6,13 +6,9 @@ import com.nix.vyrvykhvost.repository.LaptopRepository;
 import com.nix.vyrvykhvost.repository.PhoneRepository;
 import com.nix.vyrvykhvost.service.HeadphoneService;
 import com.nix.vyrvykhvost.service.LaptopService;
-import com.nix.vyrvykhvost.service.OptionalExamples;
 import com.nix.vyrvykhvost.service.PhoneService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
@@ -23,45 +19,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*PHONE_SERVICE.createAndSavePhones(2);
-        LAPTOP_SERVICE.createAndSaveLaptops(2);
-        HEADPHONE_SERVICE.createAndSaveHeadphones(2);
-
-        PHONE_SERVICE.printAll();
-        LAPTOP_SERVICE.printAll();
-        HEADPHONE_SERVICE.printAll();
+        LAPTOP_SERVICE.createAndSaveLaptops(1);
+        final String id = LAPTOP_SERVICE.getAll().get(0).getId();
+        final LaptopType type = LAPTOP_SERVICE.getAll().get(0).getType();
+        final String model = LAPTOP_SERVICE.getAll().get(0).getModel();
+        //ifPresent
+        LAPTOP_SERVICE.getPriceIfPresent(id);
+        //orElse
+        LAPTOP_SERVICE.printOrCreateGaming(id);
         LOG.info(" ");
-        Phone phone1 = new Phone("A", 1,2, "21", Manufacturer.APPLE);
-        Phone phone2 = new Phone("B", 11,22, "211", Manufacturer.APPLE);
-        PhoneRepository phoneRepository = new PhoneRepository();
-        LOG.info(phone1.toString());
-        LOG.info(phone2.toString());
-        LOG.info(phoneRepository.update(phone1));
-        LOG.info(phone1.toString());
-        LOG.info(phone2.toString());
-        LOG.info(phoneRepository.delete(phone1.getId()));
-        LOG.info(phone1.toString());
-        LOG.info(phone2.toString());*/
-
-
-        private static void optionalExamples() {
-            PHONE_SERVICE.createAndSavePhones(1);
-            final String id = PHONE_SERVICE.getAll().get(0).getId();
-            OPTIONAL_EXAMPLES.printIfPresent(id);
-            OPTIONAL_EXAMPLES.printOrGetDefault(id);
-            OPTIONAL_EXAMPLES.printOrCreatDefault(id);
-            OPTIONAL_EXAMPLES.mapPhoneToString(id);
-            OPTIONAL_EXAMPLES.printOrPrintDefault(id);
-            OPTIONAL_EXAMPLES.checksPhoneLessThen(id, 1000);
-            OPTIONAL_EXAMPLES.checksPhoneLessThen(id, 10);
-            OPTIONAL_EXAMPLES.checksPhoneLessThen("123", 1000);
-            try {
-                OPTIONAL_EXAMPLES.printPhoneOrElseThrowException(id);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-            OPTIONAL_EXAMPLES.printPhone(id);
-        }
-
+        //orElseGet
+        LAPTOP_SERVICE.printDefaultOrCreateGaming(id);
+        //map
+        LOG.info(" ");
+        LAPTOP_SERVICE.printType(id);
+        //ifPresentOrElse
+        LAPTOP_SERVICE.printLaptopWithPrice(id, 25.0);
+        //filter
+        LAPTOP_SERVICE.filterGamingLaptop(id);
+        //orElseThrow
+        LAPTOP_SERVICE.printWorkerLaptopOrThrowException(id);
+        //or
+        LAPTOP_SERVICE.printLaptopWithModel(id, "model");
     }
 }
