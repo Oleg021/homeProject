11e16b786034c489cd9b31e0855b9c7464f241a1
olehvyrvy <olehv208@gmail.com>
2022-08-26 11:35:08 +0300

    lesson 18 [add builder FIX]

--- src/main/java/com/nix/vyrvykhvost/model/Laptop.java
+++ src/main/java/com/nix/vyrvykhvost/model/Laptop.java
@@ -6,16 +6,90 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Laptop extends Product {
-    private final String model;
+   /* private final String model;
     private final Manufacturer manufacturer;
    private final LaptopType laptopType;
+
 
     public Laptop(String title, int count, double price, String model, Manufacturer manufacturer, LaptopType laptopType) {
         super(title, count, price, ProductType.LAPTOP);
         this.model = model;
         this.manufacturer = manufacturer;
         this.laptopType = laptopType;
+    }*/
+
+    private LaptopType laptopType;
+    private Manufacturer manufacturer;
+    private String model;
+
+    public Laptop() {
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setLaptopType(LaptopType laptopType) {
+        this.laptopType = laptopType;
+    }
+
+    public LaptopType getLaptopType() {
+        return laptopType;
+    }
+
+    public static class Builder {
+        private Laptop laptop;
+
+        public Builder(long price, LaptopType laptopType, Manufacturer manufacturer, String model) {
+            if (laptopType == null) {
+                throw new IllegalArgumentException("LaptopType can not be null");
+            }
+            laptop = new Laptop();
+            laptop.setPrice(price);
+            laptop.setLaptopType(laptopType);
+            laptop.setManufacturer(manufacturer);
+        }
+
+        public Builder setTittle(String title){
+            if (title.length() > 20) {
+                throw new IllegalArgumentException("Title cant be more then 20 symbols");
+            }
+            laptop.title = title;
+            return this;
+        }
+
+        public Builder setCount(int count){
+            if (count < 0) {
+                throw new IllegalArgumentException("Count cant be less then 0");
+            }
+            laptop.count = count;
+            return this;
+        }
+
+        public Builder setPrice(long price){
+            laptop.price = price;
+            return this;
+        }
+
+        public Laptop build(){
+            return laptop;
+        }
+
+    }
+
 
     @Override
     public String toString() {
